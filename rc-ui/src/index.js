e87877f8968e4d1c8d5a1f8cb503f067c5812540
Chris Knox <theknoxinator@gmail.com>
2021-08-28 13:15:18 -0400

    Add categories grid + buttons, basic add category logic

--- rc-ui/src/index.js
+++ rc-ui/src/index.js
@@ -8,9 +8,112 @@ class Rando extends React.Component {
   }
 }
 
-class Categories extends React.Component {
+function AddButton(props) {
+  return (
+    <div className="row">
+      <div className="col">
+        <button className="btn btn-primary" onClick={props.onClick}><i className="bi-plus-square"></i></button>
+      </div>
+    </div>
+  );
+}
+
+function AddSingleField(props) {
+  return (
+    <div className="row">
+      <div className="col">
+        <input type="text" value={props.value} onChange={props.onChange} />
+      </div>
+      <div className="col-4">
+        <button className="btn btn-primary mx-1" onClick={props.onSubmit}><i className="bi-plus-square"></i></button>
+        <button className="btn btn-outline-danger mx-1" onClick={props.onCancel}><i className="bi-x-square"></i></button>
+      </div>
+    </div>
+  );
+}
+
+class AddCategory extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      showField: false,
+    }
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleClick(event) {
+    const showField = this.state.showField;
+    if (!showField) {
+      this.setState({showField: true, value: ''});
+    } else {
+      const category = this.state.value;
+      this.props.onAdd(category);
+      this.setState({showField: false, value: ''});
+    }
+  }
+
+  handleCancel(event) {
+    this.setState({showField: false});
+  }
+
   render() {
-    return "Categories";
+    const showField = this.state.showField;
+    if (showField) {
+      return <AddSingleField
+               value={this.state.value}
+               onSubmit={(e) => this.handleClick(e)}
+               onChange={(e) => this.handleChange(e)}
+               onCancel={(e) => this.handleCancel(e)} />
+    } else {
+      return <AddButton onClick={() => this.handleClick()} />
+    }
+  }
+}
+
+class Categories extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: ['Board Games', 'Books', 'Movies/TV', 'Video Games'],
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleAddCategory(category) {
+    const categories = this.state.categories;
+    this.setState({
+      categories: categories.concat([category]),
+    });
+  }
+
+  render() {
+    const categories = this.state.categories;
+    const renderedCategories = categories.map((category) => {
+      return (
+        <div key={category} className="row my-2">
+          <div className="col">{category}</div>
+          <div className="col-4">
+            <button className="btn btn-primary mx-1"><i className="bi-pencil"></i></button>
+            <button className="btn btn-danger mx-1"><i className="bi-x-square"></i></button>
+          </div>
+        </div>
+      );
+    });
+    return (
+      <div className="container-md">
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          {renderedCategories}
+          <AddCategory onAdd={(e) => this.handleAddCategory(e)}/>
+        </form>
+      </div>
+    );
   }
 }
 
